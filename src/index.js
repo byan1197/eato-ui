32f596a76865315c6706987004ec8684c900b4a7
byan1197 <Bondyan97@gmail.com>
2018-03-28 17:09:01 -0400

    changed structure

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Nav from './Nav'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Nav />, document.getElementById('nav'));
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
