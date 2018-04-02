f6fbb62bbde3728605a8d18bec44bc73ebf91f15
praiyon <praiyon@gmail.com>
2018-04-01 20:00:49 -0400

    ui fix

--- src/search_bar.js
+++ src/search_bar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import Login from './Login';
+import './css/nav.css';
 
 class SearchBar extends Component{
     constructor(props){
@@ -15,45 +16,46 @@ class SearchBar extends Component{
     render(){
         return (
             <div className="Nav">
-                <div className="navbar navbar-expand-md bg-light py-2 px-5">
-                <a href="/" className="navbar-brand">eatOttawa</a>
+                <div className="navbar navbar-expand-md py-2 px-5">
+                <a href="/" className="navbar-brand">
+                    <img id="logo-img" src={require('./img/eatO-logo-final.png')} />
+                </a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar5">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="navbar-collapse collapse justify-content-stretch" id="navbar5">
-                    <ul className="navbar-nav">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">Link <span className="sr-only">Browse</span></a>
-                        </li>
-                    </ul>
                     <form className="ml-3 my-auto d-inline w-100">
                         <div className="input-group">
-                            <input type="text" 
-                                className="form-control border-right-0" 
+                            <input type="text"
+                                className="form-control border-right-0"
                                 placeholder="Search Restaurants here"
                                 value={this.state.term}
                                 onChange={event => this.onInputChange(event.target.value)}>
                             </input>
 
                             <div className="input-group-append">
-                                <button 
-                                    className="btn btn-outline-primary border-left-0" type="button"
-                                    onClick={() => this.props.onSearchTermChange(this.state.term)}>
+                                <button
+                                    className="btn btn-dark border-left-0" type="button"
+                                    onClick={() => this.props.onSearchTermChange(this.state.term)}> GO
                                 </button>
                             </div>
                         </div>
                     </form>
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <a className="btn btn-dark mx-2" href="/login">Sign In<span className="sr-only"></span>
+                            </a>
+                        </li>
+                    </ul>
                 </div>
                 </div>
             </div>
         );
-            
+
     }
 
     onInputChange(term){
         this.setState({ term });
-        
-    
     }
 }
 
